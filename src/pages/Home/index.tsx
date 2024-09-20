d416ea28e79c3fd02b1fac461ecd3917dd1d4aed
ZRMYDYCG <547471919@qq.com>
2024-09-20 09:32:35 +0800

    feat: Home页面Nav条目

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,15 @@
 const Home = () => {
-
     return (
-        <div className="text-red-500">
-            <iconpark-icon name="shezhi" width="36" height="36"></iconpark-icon>
+        <div className="p-5">
+            <header className="flex justify-end w-full">
+                <iconpark-icon name="shezhi" width="36" height="36"></iconpark-icon>
+            </header>
+            <main className="mt-5">
+                <ul className="flex">
+                    <li className="text-[35px] mr-5">笔记</li>
+                    <li className="text-gray-500 text-[35px]">代办</li>
+                </ul>
+            </main>
         </div>
     )
 }
